fda2ecc8a942f540c6f996dbf416716d17f32a7b
Berend2024 <166946793+Berend2024@users.noreply.github.com>
2026-09-02 09:20:56 +0200

    Add configuration for app settings in config.js

---
+++ .github/workflows/config.js
@@ -0,0 +1,6 @@
+window.APP_CONFIG = {
+  defaultLat: 53.3513,7   // <- deine Breite hier eintragen
+  defaultLng: 7.1806,5    // <- deine Länge hier eintragen
+  defaultFuel: "diesel",
+  defaultRadiusKm: 25
+};
